0e9a998c040dd4ac0822de7da2a32b405f2c72f4
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2016-02-14 00:48:09 -0500

    Add current primaries csv and sql script
    pulling down my spreadsheet and making a couple tables to run sql on it
    because I can't into excel/libre

---
+++ scripts/sql-table.sql
@@ -0,0 +1,29 @@
+-- run cp ../data/primaries.csv /tmp/ before running this
+
+CREATE DATABASE IF NOT EXISTS election2016;
+
+USE election2016;
+
+CREATE TABLE IF NOT EXISTS primaries (
+	state CHAR(2),
+	candidate VARCHAR(32),
+	party CHAR(3),
+	votes INT,
+	percentagevote DOUBLE,
+	delegates INT,
+	notes VARCHAR(512)
+);
+
+CREATE OR REPLACE VIEW votetotal AS
+SELECT candidate, SUM(votes) AS votes, SUM(delegates) AS delegates FROM
+primaries GROUP BY candidate;
+
+CREATE OR REPLACE VIEW partytotal AS
+SELECT party, SUM(VOTES) AS votes FROM primaries GROUP BY party;
+
+LOAD DATA INFILE '/tmp/primaries.csv'
+INTO TABLE primaries
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
