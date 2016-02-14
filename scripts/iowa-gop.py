10b934750dfcde1c75541c259ddd1e169f1bb0bc
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2016-02-13 22:22:31 -0500

    Iowa results
    Data pulled from wallstreet journal json
    
    http://graphics.wsj.com/elections/2016/iowa-caucus-results

---
+++ scripts/iowa-gop.py
@@ -0,0 +1,22 @@
+import json
+import os
+
+datafile = '../data/iowa-gop.json'
+with open(datafile) as f:
+	data = json.load(f)
+
+candidateVotes = {}
+for countyCode in data['data']:
+	county = data['data'][countyCode]
+	if county['county'] == 'Iowa':
+		continue # Don't count the aggregate
+	else:
+		for cand in county['Cand']:
+			votes = cand['votes']
+			name = cand['name']
+			if name in candidateVotes:
+				candidateVotes[name] = candidateVotes[name] + votes
+			else:
+				candidateVotes[name] = votes
+
+print candidateVotes
